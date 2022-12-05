e6967f9821d2773ea961e25441c0c5218e637eeb
sbstobiask <sbs202223kerchner@gmail.com>
2022-12-05 12:11:48 +0100

    Initial commit

---
+++ Bongosort.py
@@ -0,0 +1,15 @@
+import copy
+import random
+
+x=0
+deck1 = [x + 1 for x in range(8)]
+deck2 = copy.deepcopy(deck1)
+random.shuffle(deck2)
+
+while True:
+    x += 1
+    random.shuffle(deck2)
+    if deck1 == deck2:
+        print("Nach", x, "Versuchen waren die Karten wieder in der richtigen Reihenfolge")
+        break
+
